1857193eaf14f311cb5f8894ce2eca1ea353fd24
pxl <357908328@qq.com>
2020-08-09 15:17:23 +0800

    导入播放电影页面的静态文件

--- aiyiqi_app/resources/movies/__init__.py
+++ aiyiqi_app/resources/movies/__init__.py
@@ -5,6 +5,7 @@ from resources.movies import movie
 
 movie_bp = Blueprint('movies', __name__)
 
-api.add_resource(movie.MovieList, '/movieList', endpoint='MovieList')
+api.add_resource(movie.MovieList, '/movieList', endpoint='movieList')
+api.add_resource(movie.MoviePlay, '/moviePlay', endpoint='moviePlay')
 api.add_resource(movie.ppp, '/ppp', endpoint='ppp')
 api.add_resource(movie.hello, '/hello', endpoint='hello')
--- aiyiqi_app/resources/movies/movie.py
+++ aiyiqi_app/resources/movies/movie.py
@@ -1,6 +1,7 @@
 from flask import render_template, jsonify
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from sqlalchemy import text
+from sqlalchemy.exc import SQLAlchemyError
 
 from models.movie import CategoryMovie, MovieTable
 from models import db_session
@@ -27,9 +28,42 @@ class MovieList(Resource):
             all_list.append(dict1)
 
         # 电影网格列表
-        movies = MovieTable.query.all()
+        rp = reqparse.RequestParser()
+        rp.add_argument('page', type=int, location=['args'], default=1)
+        rp.add_argument('pagesize', type=int, location=['args'], default=30)
+        params = rp.parse_args()
+        pagesize = params.pagesize
+        offset = (params.page - 1) * pagesize
+        # movies = MovieTable.query.order_by(MovieTable.score.desc()).offset(offset).limit(pagesize).all()
+        movies = MovieTable.query.offset(offset).limit(pagesize).all()
+        total = len(MovieTable.query.all())
+        if total % pagesize == 0:
+            total_page = total / pagesize
+        else:
+            total_page = total / pagesize + 1
+        return render_template('movielist.html', cates=all_list, movies=movies, total=total, page_num=params.page,
+                               total_page=int(total_page))
 
-        return render_template('movielist.html', cates=all_list, movies=movies,movie_count=len(movies))
+
+class MoviePlay(Resource):
+    def get(self):
+        rp = reqparse.RequestParser()
+        rp.add_argument('movieid', location='args', required=True)
+        rp.add_argument('playhtml', location='args')
+        params = rp.parse_args()
+        movie_id = params.movieid
+
+        movie_id = 858
+
+        # stmt = text(
+        #     "SELECT md.id,mt.score,mt.moviename,md.director,group_concat(mp.performer) AS perman,group_concat(mp.role) AS perrole \
+        # FROM movietable AS mt INNER JOIN (moviedetailtable AS md,movieperformertable AS mp) ON \
+        # mt.id = md.id AND mt.id = mp.id AND md.id = mp.id \
+        # WHERE md.id =:x GROUP BY md.id")
+        #
+        # groups = db_session.execute(stmt, params={'x': movie_id}).first()
+
+        return render_template('movieplay.html')
 
 
 class ppp(Resource):
